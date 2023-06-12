69c7863822f0fc85e719af9de5dd16ade1bbe771
user <user@DESKTOP-D0AOBF6>
2023-06-12 12:26:58 +0900

    shs

---
+++ korail/src/main/java/com/korail/service/CardinfoServiceImpl.java
@@ -0,0 +1,22 @@
+package com.korail.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.korail.dao.CardinfoDao;
+import com.korail.vo.CardinfoVo;
+
+@Service
+public class CardinfoServiceImpl {
+   @Autowired
+   private CardinfoDao cardinfoDao;
+   
+   
+   public int getPayment(CardinfoVo cardVo) {
+      return cardinfoDao.insert(cardVo);
+   }
+   
+   public int getPaymentUpdate(CardinfoVo cardVo) {
+      return cardinfoDao.insertUpdate(cardVo);
+   }
+}
