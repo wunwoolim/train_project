1d7892d9de9a4304c8b5ccfe268ebc9d6a55bfe1
nihao <nihao@DESKTOP-HJN26T6>
2023-06-20 20:26:08 +0900

    min

--- korail/src/main/java/com/korail/controller/TicketController.java
+++ korail/src/main/java/com/korail/controller/TicketController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class TicketController {a
+public class TicketController {
 	
 	@RequestMapping(value="/info_payment.do", method=RequestMethod.GET)
 	public String payment_info() {
