67bc0e839ee40baf1a71de9d21afe65232c60ed2
jeonggeon <kjkkmk42397@gmail.com>
2023-06-23 11:41:19 +0900

    nihao

--- korail/src/main/java/com/korail/service/MailSendService.java
+++ korail/src/main/java/com/korail/service/MailSendService.java
@@ -35,7 +35,7 @@ public class MailSendService {
 			String content = 
 					"홈페이지를 방문해주셔서 감사합니다." + 	//html 형식으로 작성 ! 
 	                "<br><br>" + 
-				    "인증 번호는 " + authNumber + "입니다." + 
+				    "인증 번호는 <span style='color:blue;' ,'text-decoration:none;'>" + authNumber + "</span>입니다." + 
 				    "<br>" + 
 				    "해당 인증번호를 인증번호 확인란에 기입하여 주세요."; //이메일 내용 삽입
 			mailSend(setFrom, toMail, title, content);
